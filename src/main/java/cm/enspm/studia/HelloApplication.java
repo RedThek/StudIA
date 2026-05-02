8acc49c6f8435dcc59564c26354145bbe85efd86
MOLA <91349290+RedThek@users.noreply.github.com>
2026-05-02 03:48:03 +0100

    feat: Enhance UI with new FXML views, CSS styles, and favicon images

--- src/main/java/cm/enspm/studia/HelloApplication.java
+++ src/main/java/cm/enspm/studia/HelloApplication.java
@@ -2,7 +2,9 @@ package cm.enspm.studia;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -10,8 +12,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 720);
-        stage.setTitle("StudIA - Gestion scolaire bilingue");
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("StudIA");
+        try{
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("img/favicon/StudIA-couleur-fav-16.png")));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         stage.setScene(scene);
         stage.show();
     }
